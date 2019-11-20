276b0a29a2ba61d7498aa77784172606cbd89dea
obiel <obiel@CDA-AR-0144.cdainfo.local>
2019-11-20 11:30:34 -0300

    Envido 2.0

--- Javote/src/cartitas/Truco.java
+++ Javote/src/cartitas/Truco.java
@@ -23,6 +23,7 @@ public class Truco {
 		String palo1 = j.getMano().get(0).getPalo();
 		String palo2 = j.getMano().get(1).getPalo();
 		String palo3 = j.getMano().get(2).getPalo();
+		
 		int tanto;
 		
 		if(palo1.equals(palo2)) {
@@ -31,12 +32,9 @@ public class Truco {
 			tanto = verificarTantoEnvido(j.getMano().get(1),j.getMano().get(2));
 		} else if (palo1.contentEquals(palo3)) {
 			tanto = verificarTantoEnvido(j.getMano().get(0),j.getMano().get(2));
-		} else {
-			System.out.println("No tiene palos iguales ¿Que carta desea jugar?");
-			Scanner ingreso = new Scanner(System.in);
-			tanto = j.getMano().get(ingreso.nextInt()).getNumero();
+		} else { tanto = verificarCartaMasAlta(j.getMano().get(0),j.getMano().get(1),j.getMano().get(2));
+			
 		}
-		
 		System.out.println("Tengo "+tanto+" de tanto");
 	}
 	
@@ -47,12 +45,22 @@ public class Truco {
 		if(valor1 == 10 || valor1 == 11 || valor1 == 12) {
 			valor1=0;
 		}
-		
 		if(valor2 == 10 || valor2 == 11 || valor2 == 12) {
 			valor2=0;
 		}
-		
 		return 20 + valor1 + valor2 ;
+	}
+	
+	public int verificarCartaMasAlta(Cartita carta1, Cartita carta2, Cartita carta3) {
+		int num1 = carta1.getNumero();
+		int num2 = carta2.getNumero();
+		int num3 = carta3.getNumero();
+		
+		if(num1>num2 && num1>num3) {
+			return num1;
+		} else if(num2>num1 && num2>num3) {
+			return num2;
+		} else return num3;
 		
 	}
 	
